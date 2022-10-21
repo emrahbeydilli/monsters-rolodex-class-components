063a574e0343512183acbf804378cb30bd5b8c2f
emrahbeydilli <emrahbeydilli@gmail.com>
2022-10-21 21:01:13 +0300

    updating App component

--- src/App.js
+++ src/App.js
@@ -4,28 +4,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      name: {firstName:"Emrah", lastName:"Beydilli"},
-      company: "Ağlasun MYO"
+      monsters: [
+        {
+          name: "Linda"
+        },
+        {
+          name: "Frank"
+        },
+        {
+          name: "Jacky"
+        },
+      ]
     }
   }
-  render(){
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}! I work at {this.state.company}
-          </p>
-          <button onClick={()=>{
-            this.setState({name: {firstName:"Arda", lastName:"Erol"}});
-            console.log(this.state)
-          }}>
-            Change Name
-          </button>
-        </header>
+        {
+          this.state.monsters.map((monster)=>{
+            return <h1 key={monster.name}>{monster.name}</h1>
+          })
+        }
       </div>
     );
   }
