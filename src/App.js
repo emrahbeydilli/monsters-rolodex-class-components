813ced6457c859336342181be9b14de57fb1e0fe
emrahbeydilli <emrahbeydilli@gmail.com>
2022-11-03 20:12:54 +0300

    updating app component

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ class App extends Component {
     this.state = {
       monsters: [],
     }
-    console.log("constructor");
   }
   componentDidMount() {
-    console.log("componentDidMount");
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) =>
@@ -27,7 +25,6 @@ class App extends Component {
       )
   }
   render() {
-    console.log("render");
     return (
       <div className="App">
         {
